e6f0ebff951eae88ff0cf1ecffe1d645ad820264
n_206 <nandornotfound@gmail.com>
2026-02-13 13:00:50 +0100

    minesweeper innitial commit

---
+++ 260213/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _260213
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        SubWindow subWindow = new SubWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            subWindow.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            subWindow.Close();
+        }
+
+        public void ShowSubWindow()
+        {
+            subWindow = new SubWindow();
+            subWindow.Show();
+            subWindow.Owner = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            subWindow.Owner = this;
+        }
+    }
+
+    public class Main
+    {
+        int flags = 0;
+        int time = 0;
+        int mines = 0;
+        public void InitGame()
+        {
+
+        }
+    }
+}
---
+++ 260213/SubWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _260213
+{
+    /// <summary>
+    /// Interaction logic for SubWindow.xaml
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (!(PresentationSource.FromVisual(this.Owner) == null))
+            {
+                ((MainWindow)this.Owner).ShowSubWindow();
+            }
+        }
+        private int _numValue = 7;
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumValue <= 10 * 11 / 3)
+            {
+                NumValue++;
+            }
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumValue > 3)
+            {
+                NumValue--;
+            }
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+
+    }
+}
